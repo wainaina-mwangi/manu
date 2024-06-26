39b524fa48811f7980d44974d922078957fd5cb1
wainaina-mwangi <164462952+wainaina-mwangi@users.noreply.github.com>
2024-06-26 03:09:17 -0700

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+ /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
